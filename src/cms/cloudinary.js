5b1a693ef3204ca4e460a8d328b5864a1887f6ad
Shawn Erquhart <shawn@erquh.art>
2018-09-10 16:31:59 -0400

    add pre-release cloudinary integration

---
+++ src/cms/cloudinary.js
@@ -0,0 +1,39 @@
+import { loadScript } from 'netlify-cms-lib-util';
+
+/**
+ * This configuration hash cannot be overriden, as the values here are required
+ * for the integration to work properly.
+ */
+const enforcedConfig = {
+  button_class: undefined,
+  inline_container: undefined,
+  insert_transformation: false,
+  z_index: '99999',
+};
+
+const defaultConfig = {
+  multiple: false,
+};
+
+async function init({ options = { config: {} }, handleInsert }) {
+  const cloudinaryConfig = { ...defaultConfig, ...options.config, ...enforcedConfig };
+
+  await loadScript('https://media-library.cloudinary.com/global/all.js');
+
+  const insertHandler = data => {
+    const assets = data.assets.map(a => options.useSecureUrl ? a.secure_url : a.url);
+    handleInsert(cloudinaryConfig.multiple ? assets : assets[0]);
+  }
+
+  const mediaLibrary = window.cloudinary.createMediaLibrary(cloudinaryConfig, { insertHandler });
+
+  return {
+    show: () => mediaLibrary.show(),
+    hide: () => mediaLibrary.hide(),
+    enableStandalone: () => true,
+  };
+}
+
+const cloudinaryMediaLibrary = { name: 'cloudinary', init };
+
+export default cloudinaryMediaLibrary;
